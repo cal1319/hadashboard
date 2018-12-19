685601f0a14d359004f840c9357746617bbf927f
cal1319 <oocchiverdio@gmail.com>
2018-12-19 14:46:32 -0800

    Update radar.rb

--- jobs/radar.rb
+++ jobs/radar.rb
@@ -1,101 +1,33 @@
-require "net/http"
-require "json"
+require 'open-uri'
 
+@cameraDelay = 1 # Needed for image sync. 
+@fetchNewImageEvery = '180s'
 
-# If these are left blank, the script will try to locate you using your IP address
-latitude = "38.5378863"                   # Required
-longitude = "-121.57037609999998"                  # Required
-location = "West Sacramento,<us>USA"   # Change me
+@newFile1 = "assets/images/radar/new.jpg"
+@oldFile1 = "assets/images/radar/old.jpg"
 
-units = "us"
-symbol = "F"
-key = "353b2c70b063c646e905d67cc2867088"                        # Required
-
-def ftoc (f)
-    c = (f)
+# Change "OHX" in the file << open... line to your radar station ID. Check README for link.
+def fetch_image(old_file,new_file)
+	`rm #{old_file}` 
+	`mv #{new_file} #{old_file}`	
+	open('assets/images/radar/new.jpg', 'wb') do |file|
+		file << open('https://cdns.abclocal.go.com/three/kgo/weather/maps/ncal1_1280.jpg').read
+	end
+	new_file
 end
 
-    
-SCHEDULER.every "15m", :first_in => 0 do |job|
+def make_web_friendly(file)
+  "/" + File.basename(File.dirname(file)) + "/" + File.basename(file)
+end
 
-    uri = URI("https://api.darksky.net/forecast/#{key}/#{latitude},#{longitude}?units=#{units}")
-    req = Net::HTTP::Get.new(uri.path)
+SCHEDULER.every @fetchNewImageEvery, first_in: 0 do
+	new_file1 = fetch_image(@oldFile1,@newFile1)
 
-    # Make request
-    res = Net::HTTP.start(
-            uri.host, uri.port, 
-            :use_ssl => uri.scheme == 'https', 
-            :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
-      https.request(req)
-    end
-
-    response = JSON.parse res.body
-    
-    currentResults = response["currently"]
-    dailyResults  = response["daily"]["data"]
-        
-    forecasts = []
-    
-    #Today
-    today = {}
-    
-    if currentResults
-        
-        currentTemp = symbol == "F" ? "#{ftoc(currentResults["temperature"]).round}°#{symbol}" : "#{currentResults["temperature"].round}°#{symbol}"
-        currentlyIcon = currentResults["icon"]
-        currentHigh = ftoc(dailyResults[0]["temperatureMax"]).round
-        currentLow = ftoc(dailyResults[0]["temperatureMin"]).round
-        currentSummary = response["hourly"]["summary"]
-        todaysSummary = "High of #{currentHigh} with a low of #{currentLow}. #{currentSummary}"
-            
-        # Create object for this current day
-        today = {
-                temp: currentTemp,
-                summary:  todaysSummary,
-                code: currentlyIcon,
-                element: 'currentWeatherIcon',
-                location: location
-            }
-
-    end
-    
-    #Future Days
-
-    if dailyResults
-
-        # Create weather object for the next 5 days
-        for day in (1..5) 
-        
-            day = dailyResults[day]
-        
-            # Format date as a qualified day i.e. Monday
-            time = Time.at(day["time"]).strftime("%A")
-            summary = day["summary"]
-            
-            # Should it be displayed in Celsius? If not, display in Fahrenheit
-            if(symbol == "C")
-                min = ftoc(day["temperatureMin"])
-                max = ftoc(day["temperatureMax"])
-            else
-                min = day["temperatureMin"]
-                max = day["temperatureMax"]
-            end
-                
-            # Create object for the day to send back to the widget
-            this_day = {
-                high: max.round,
-                low:  min.round,
-                date: time,
-                code: day["icon"],
-                text: day["text"], 
-                
-                element: 'weather-icon'
-            }
-            forecasts.push(this_day)
-        end
-
-        send_event "weeklyweather", { forecasts: forecasts, today: today }
-    end
-
-
+	if not File.exists?(@newFile1)
+		warn "Failed to Get Radar Image"
+	end
+ 
+	send_event('radar', image: make_web_friendly(@oldFile1))
+	sleep(@cameraDelay)
+	send_event('radar', image: make_web_friendly(new_file1))
 end
