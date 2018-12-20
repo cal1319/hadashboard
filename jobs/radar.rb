eef168aed1c9fae7567ac17457443a4fd3d783da
cal1319 <oocchiverdio@gmail.com>
2018-12-19 16:57:38 -0800

    Update radar.rb

--- jobs/radar.rb
+++ jobs/radar.rb
@@ -1,36 +1,21 @@
+require 'net/http'
 require 'open-uri'
 
-@cameraDelay = 1 # Needed for image sync. 
-@fetchNewImageEvery = '15s'
+@url = 'https://cdns.abclocal.go.com/three/kgo/weather/maps/ncal1_1280.jpg'
 
-@newFile1 = "assets/images/radar/new.jpg"
-@oldFile1 = "assets/images/radar/old.jpg"
+SCHEDULER.every '4s', :first_in => 0 do |job|
 
-	
+    `find '/assets/images/radar/' -type f -mmin +1 -print0 | xargs -0 rm -f`
 
-# Change "OHX" in the file << open... line to your radar station ID. Check README for link.
-def fetch_image(old_file,new_file)
-	`rm #{old_file}` 
-	`mv #{new_file} #{old_file}`	
-	open('assets/images/radar/new.jpg', 'wb') do |file|
-		file << open('https://cdns.abclocal.go.com/three/kgo/weather/maps/ncal1_1280.jpg').read
-	end
-	new_file
-end
+    @currentTime = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
+    @newFile1 = "assets/images/radar/snapshot-" + @currentTime + "_new.jpeg"
 
-def make_web_friendly(file)
-  "/" + File.basename(File.dirname(file)) + "/" + File.basename(file)
-end
+    open(@url, :http_basic_authentication => ['root', 'CamPW']) do |f|
+      open(@newFile1,'wb') do |file|
+        file.puts f.read
+      end
+    end
 
-SCHEDULER.every "30s", :first_in => 0 do
-	new_file1 = fetch_image(@oldFile1,@newFile1)
+    send_event('radar', image: ("/" + File.basename(File.dirname(@newFile1)) + "/" + File.basename(@newFile1)))
 
-	if not File.exists?(@newFile1)
-		warn "Failed to Get Radar Image"
-end
-
- 
-	send_event('radar', image: make_web_friendly(@oldFile1))
-	sleep(@cameraDelay)
-	send_event('radar', image: make_web_friendly(new_file1))
 end
