35313e3b92c5fed0e717a9e9dc9f470967cdcbee
cal1319 <oocchiverdio@gmail.com>
2019-03-15 17:10:52 -0700

    Update forecast.rb

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -39,14 +39,15 @@ SCHEDULER.every '180m', :first_in => 0 do |job|
     forecast_current_icon = forecast["currently"]["icon"]
     forecast_current_desc = forecast["currently"]["summary"]
     if forecast["minutely"]
-         forecast_next_desc  = forecast["minutely"]["summary"]
-         forecast_next_icon  = forecast["minutely"]["icon"]
+       forecast_next_desc  = forecast["minutely"]["summary"]
+       forecast_next_icon  = forecast["minutely"]["icon"]
     else
-      puts "Did not get minutely forecast data again"
-      forecast_next_desc  = "No data"
-      forecast_next_icon  = ""
-  end
-  forecast_later_desc   = forecast["hourly"]["summary"]
-  forecast_later_icon   = forecast["hourly"]["icon"]
-  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+       puts "Did not get minutely forecast data again"
+       forecast_next_desc  = "No data"
+       forecast_next_icon  = ""
+   end
+       forecast_later_desc   = forecast["hourly"]["summary"]
+       forecast_later_icon   = forecast["hourly"]["icon"]
+       send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+   end
 end
