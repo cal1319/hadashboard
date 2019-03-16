7ee5824507c2c252a166a015de6f96ced6177360
cal1319 <oocchiverdio@gmail.com>
2019-03-15 17:06:44 -0700

    Update forecast.rb

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -32,19 +32,21 @@ SCHEDULER.every '180m', :first_in => 0 do |job|
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
     response = http.request(Net::HTTP::Get.new(uri.request_uri))
-p    if not response.kind_of? Net::HTTPSuccess then raise "Forecast HTTP error" end
+    if not response.kind_of? Net::HTTPSuccess then raise "Forecast HTTP error" end
     forecast = JSON.parse(response.body)
     if forecast.has_key?("code") then raise "Forecast Error: #{forecast['code']}: #{forecast['error']}" end
     forecast_current_temp = forecast["currently"]["temperature"].round
     forecast_current_icon = forecast["currently"]["icon"]
-    if forecast.has_key?("minutely") then forecast_summary_key = "minutely"
-    elsif forecast.has_key?("houly") then forecast_summary_key = "hourly"
-    elsif forecast.has_key?("daily") then forecast_summary_key = "daily"
-    else                                  forecast_summary_key = "currently" end
-    forecast_summary = forecast[forecast_summary_key]["summary"]
-    send_event('forecast', { temperature: "#{forecast_current_temp}&deg;", hour: "#{forecast_summary}"})
-  rescue => e
-    puts "\e[33mFor the forecast widget to work, you need to set up your API key and coordinates in jobs/forecast.rb file.\e[0m"
-    puts "\e[33m#{e.message}\e[0m"
+    forecast_current_desc = forecast["currently"]["summary"]
+    if forecast["minutely"]
+         forecast_next_desc  = forecast["minutely"]["summary"]
+         forecast_next_icon  = forecast["minutely"]["icon"]
+    else
+      puts "Did not get minutely forecast data again"
+      forecast_next_desc  = "No data"
+      forecast_next_icon  = ""
   end
+  forecast_later_desc   = forecast["hourly"]["summary"]
+  forecast_later_icon   = forecast["hourly"]["icon"]
+  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
 end
